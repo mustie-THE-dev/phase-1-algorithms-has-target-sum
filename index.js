c2b847b3e3f278b368d69a7774516cfb0dd600f4
Simon kamau <simon.kamau@student.moringaschool.com>
2022-06-21 18:40:39 +0300

    commit tree 58599de147e5ed04be91af1bd7fb75412fc4ac09

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    
+    const num1 = target - array[i];
+    //loop through the rest of the array 
+    for (let j = i + 1; j < array.length; j++) {
+
+      //Check if total matches target
+      if (array[j] === num1) {
+        return true
+      } 
+
+    }
+  } 
+  return false
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+console.log(hasTargetSum([3,8,12,4,11,7],10));
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25));
+console.log( hasTargetSum([1, 2, 5], 4));
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
